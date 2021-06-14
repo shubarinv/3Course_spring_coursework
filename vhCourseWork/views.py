017534651d886d9bb0be3e0f6db780b9e6bd72f9
Vladimir Shubarin <vhundef@gmail.com>
2021-06-14 18:04:14 +0300

    added ability to add clients

--- vhCourseWork/views.py
+++ vhCourseWork/views.py
@@ -36,7 +36,8 @@ def client_edit(request, client_id):
         client_data.director = request.POST.get('director')
         client_data.address = request.POST.get('address')
         client_data.phone = request.POST.get('phone')
-        client_data.TIN = request.POST.get('TIN')
+        client_data.tin = request.POST.get('tin')
+        client_data.bank_details = request.POST.get('bank_details')
         client_data.notes = request.POST.get('notes')
         client_data.save()
         return HttpResponseRedirect('/clients')
@@ -48,12 +49,25 @@ def client_edit(request, client_id):
         return HttpResponse(template.render(context, request))
 
 
-
-
 def client_remove(request, client_id):
-    client_data = get_object_or_404(Client, pk=client_id)
-    template = loader.get_template('client/client_remove.html')
-    context = {
-        'client': client_data,
-    }
-    return HttpResponse(template.render(context, request))
+    client_data = get_object_or_404(Client, pk=client_id).delete()
+    return HttpResponseRedirect('/clients')
+
+
+def client_add(request):
+    if request.method == 'POST':
+        client_data = Client()
+        client_data.name = request.POST.get('name')
+        client_data.director = request.POST.get('director')
+        client_data.address = request.POST.get('address')
+        client_data.phone = request.POST.get('phone')
+        client_data.bank_details = request.POST.get('bank_details')
+        client_data.tin = request.POST.get('tin')
+        client_data.notes = request.POST.get('notes')
+        client_data.save()
+        return HttpResponseRedirect('/clients')
+    else:
+        template = loader.get_template('client/client_edit.html')
+        context = {
+        }
+        return HttpResponse(template.render(context, request))
