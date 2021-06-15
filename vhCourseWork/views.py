c1c82ca04d72dc47d8825cacbed7f510dd49b116
Vladimir Shubarin <vhundef@gmail.com>
2021-06-15 13:55:52 +0300

    added client/manufacturer/supplier add/remove/edit/view

--- vhCourseWork/views.py
+++ vhCourseWork/views.py
@@ -13,7 +13,7 @@ def index(request):
 
 def client(request, client_id):
     client_data = get_object_or_404(Client, pk=client_id)
-    template = loader.get_template('client/client.html')
+    template = loader.get_template('organisation/organisation_details.html')
     context = {
         'client': client_data,
     }
@@ -22,8 +22,9 @@ def client(request, client_id):
 
 def clients(request):
     clients_data = Client.objects.all()
-    template = loader.get_template('client/clients.html')
+    template = loader.get_template('organisation/organisations_list.html')
     context = {
+        'org_type': "client",
         'clients': clients_data,
     }
     return HttpResponse(template.render(context, request))
@@ -42,9 +43,10 @@ def client_edit(request, client_id):
         client_data.save()
         return HttpResponseRedirect('/clients')
     else:
-        template = loader.get_template('client/client_edit.html')
+        template = loader.get_template('organisation/organisation_data_manipulation.html')
         context = {
-            'client': client_data,
+            'org_type': "client",
+            'org_data': client_data,
         }
         return HttpResponse(template.render(context, request))
 
@@ -67,7 +69,146 @@ def client_add(request):
         client_data.save()
         return HttpResponseRedirect('/clients')
     else:
-        template = loader.get_template('client/client_edit.html')
+        template = loader.get_template('organisation/organisation_data_manipulation.html')
         context = {
+            'org_type': "client",
         }
         return HttpResponse(template.render(context, request))
+
+
+def manufacturers(request):
+    manufacturers_data = Manufacturer.objects.all()
+    template = loader.get_template('organisation/organisations_list.html')
+    context = {
+        'org_type': "manufacturer",
+        'manufacturers': manufacturers_data,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def manufacturer(request, manufacturer_id):
+    manufacturer_data = get_object_or_404(Manufacturer, pk=manufacturer_id)
+    template = loader.get_template('organisation/organisation_details.html')
+    context = {
+        'manufacturer': manufacturer_data,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def manufacturer_add(request):
+    if request.method == 'POST':
+        manufacturer_data = Manufacturer()
+        manufacturer_data.name = request.POST.get('name')
+        manufacturer_data.director = request.POST.get('director')
+        manufacturer_data.accountant = request.POST.get('accountant')
+        manufacturer_data.address = request.POST.get('address')
+        manufacturer_data.phone = request.POST.get('phone')
+        manufacturer_data.fax = request.POST.get('fax')
+        manufacturer_data.bank_details = request.POST.get('bank_details')
+        manufacturer_data.tin = request.POST.get('tin')
+        manufacturer_data.notes = request.POST.get('notes')
+        manufacturer_data.save()
+        return HttpResponseRedirect('/manufacturers')
+    else:
+        template = loader.get_template('organisation/organisation_data_manipulation.html')
+        context = {
+            'org_type': "manufacturer",
+        }
+        return HttpResponse(template.render(context, request))
+
+
+def manufacturer_edit(request, manufacturer_id):
+    manufacturer_data = get_object_or_404(Manufacturer, pk=manufacturer_id)
+    if request.method == 'POST':
+        manufacturer_data.name = request.POST.get('name')
+        manufacturer_data.director = request.POST.get('director')
+        manufacturer_data.accountant = request.POST.get('accountant')
+        manufacturer_data.address = request.POST.get('address')
+        manufacturer_data.phone = request.POST.get('phone')
+        manufacturer_data.fax = request.POST.get('fax')
+        manufacturer_data.bank_details = request.POST.get('bank_details')
+        manufacturer_data.tin = request.POST.get('tin')
+        manufacturer_data.notes = request.POST.get('notes')
+        manufacturer_data.save()
+        return HttpResponseRedirect('/manufacturers')
+    else:
+        template = loader.get_template('organisation/organisation_data_manipulation.html')
+        context = {
+            'org_type': "manufacturer",
+            'org_data': manufacturer_data,
+        }
+        return HttpResponse(template.render(context, request))
+
+
+def manufacturer_remove(request, manufacturer_id):
+    manufacturer_data = get_object_or_404(Manufacturer, pk=manufacturer_id).delete()
+    return HttpResponseRedirect('/manufacturers')
+
+
+def suppliers(request):
+    suppliers_data = Supplier.objects.all()
+    template = loader.get_template('organisation/organisations_list.html')
+    context = {
+        'org_type': "supplier",
+        'suppliers': suppliers_data,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def supplier_add(request):
+    if request.method == 'POST':
+        supplier_data = Supplier()
+        supplier_data.name = request.POST.get('name')
+        supplier_data.director = request.POST.get('director')
+        supplier_data.accountant = request.POST.get('accountant')
+        supplier_data.address = request.POST.get('address')
+        supplier_data.phone = request.POST.get('phone')
+        supplier_data.fax = request.POST.get('fax')
+        supplier_data.bank_details = request.POST.get('bank_details')
+        supplier_data.tin = request.POST.get('tin')
+        supplier_data.notes = request.POST.get('notes')
+        supplier_data.save()
+        return HttpResponseRedirect('/suppliers')
+    else:
+        template = loader.get_template('organisation/organisation_data_manipulation.html')
+        context = {
+            'org_type': "supplier",
+        }
+        return HttpResponse(template.render(context, request))
+
+
+def supplier_edit(request, supplier_id):
+    supplier_data = get_object_or_404(Supplier, pk=supplier_id)
+    if request.method == 'POST':
+        supplier_data.name = request.POST.get('name')
+        supplier_data.director = request.POST.get('director')
+        supplier_data.accountant = request.POST.get('accountant')
+        supplier_data.address = request.POST.get('address')
+        supplier_data.phone = request.POST.get('phone')
+        supplier_data.fax = request.POST.get('fax')
+        supplier_data.bank_details = request.POST.get('bank_details')
+        supplier_data.tin = request.POST.get('tin')
+        supplier_data.notes = request.POST.get('notes')
+        supplier_data.save()
+        return HttpResponseRedirect('/suppliers')
+    else:
+        template = loader.get_template('organisation/organisation_data_manipulation.html')
+        context = {
+            'org_type': "supplier",
+            'org_data': supplier_data,
+        }
+        return HttpResponse(template.render(context, request))
+
+
+def supplier_remove(request, supplier_id):
+    supplier_data = get_object_or_404(Supplier, pk=supplier_id).delete()
+    return HttpResponseRedirect('/suppliers')
+
+
+def supplier(request, supplier_id):
+    supplier_data = get_object_or_404(Supplier, pk=supplier_id)
+    template = loader.get_template('organisation/organisation_details.html')
+    context = {
+        'supplier': supplier_data,
+    }
+    return HttpResponse(template.render(context, request))
